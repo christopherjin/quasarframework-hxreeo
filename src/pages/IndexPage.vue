051288513773633c2f3d489322ce6ded0be542a7
christopherjin <christopherjin@gmail.com>
2022-07-09 19:20:54 -0700

    1 file modified, 1 added

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,12 @@
 <template>
   <q-page class="center">
-    <q-video
-      :ratio="560 / 315"
-      src="https://www.youtube.com/embed/jUQ_tt_zJDo"
-    />
+    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+      <dotted-bar-chart></dotted-bar-chart>
+    </div>
   </q-page>
 </template>
 
 <script setup>
 import { ref } from 'vue';
-
-const count = ref(0);
-
-const min = -5;
-const max = 5;
+import DottedBarChart from 'components/DottedBarChart';
 </script>
