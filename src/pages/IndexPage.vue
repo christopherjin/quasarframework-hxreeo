3c07d3fa29d389a1661bf06594ba2cddc005291f
christopherjin <christopherjin@gmail.com>
2022-07-09 18:59:45 -0700

    1 file modified

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,35 +1,9 @@
 <template>
-  <q-page class="column flex-center">
+  <q-page class="center">
     <q-video
       :ratio="560 / 315"
       src="https://www.youtube.com/embed/jUQ_tt_zJDo"
     />
-
-    <div class="q-mt-xl">
-      <q-btn
-        color="primary"
-        dense
-        round
-        label="-"
-        :disable="count === min"
-        @click="count--"
-      />
-
-      <span class="q-mx-md text-bold">{{ count }}</span>
-
-      <q-btn
-        color="primary"
-        dense
-        round
-        label="+"
-        :disable="count === max"
-        @click="count++"
-      />
-    </div>
-
-    <div class="q-mt-md" style="width: 200px">
-      <q-slider v-model="count" :min="min" :max="max" />
-    </div>
   </q-page>
 </template>
 
