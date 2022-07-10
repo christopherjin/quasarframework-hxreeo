b4e7c92641bd5ad83bd71307bfb9c4597cc97a9d
christopherjin <christopherjin@gmail.com>
2022-07-09 19:30:38 -0700

    1 file modified, 1 deleted

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -4,15 +4,21 @@
       :ratio="560 / 315"
       src="https://www.youtube.com/embed/jUQ_tt_zJDo"
     />
-  </q-page>
-  <q-page class="">
-    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-      <dotted-bar-chart></dotted-bar-chart>
-    </div>
+
+    <q-card>
+      <q-card-section class="text-h6"> Dotted Bar Chart </q-card-section>
+      <q-card-section>
+        <div
+          ref="dottedbarchart"
+          id="dottedbarChart"
+          style="height: 250px"
+        ></div>
+      </q-card-section>
+      <q-resize-observer @resize="onResize" />
+    </q-card>
   </q-page>
 </template>
 
 <script>
 import { ref } from 'vue';
-import DottedBarChart from 'components/DottedBarChart';
 </script>
