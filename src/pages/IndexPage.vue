60b88bc3d6359422fb917059e6023b9f35ea5c1a
christopherjin <christopherjin@gmail.com>
2022-07-13 19:41:12 -0700

    3 files modified

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -4,19 +4,8 @@
       :ratio="560 / 315"
       src="https://www.youtube.com/embed/jUQ_tt_zJDo"
     />
-
-    <q-card>
-      <q-card-section class="text-h6"> Dotted Bar Chart </q-card-section>
-      <q-card-section>
-        <div
-          ref="dottedbarchart"
-          id="dottedbarChart"
-          style="height: 250px"
-        ></div>
-      </q-card-section>
-      <q-resize-observer @resize="onResize" />
-    </q-card>
   </q-page>
+  <q-button></q-button>
 </template>
 
 <script>
---
+++ src/pages/Progress.vue
@@ -0,0 +1,9 @@
+<template>
+  <q-card>
+    <q-card-section class="text-h6"> Dotted Bar Chart </q-card-section>
+    <q-card-section>
+      <div ref="dottedbarchart" id="dottedbarChart" style="height: 250px"></div>
+    </q-card-section>
+    <q-resize-observer @resize="onResize" />
+  </q-card>
+</template>
