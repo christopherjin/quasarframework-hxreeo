d3516ee5b6b3a632a58cca2960ef4b2a1841cc1c
christopherjin <christopherjin@gmail.com>
2022-07-18 14:48:53 -0700

    3 files modified, 1 added, 1 deleted

--- src/boot/apexchart.js
+++ src/boot/apexchart.js
@@ -1,4 +1,6 @@
 import VueApexCharts from 'vue3-apexcharts';
+import { boot } from 'quasar/wrappers';
 
-const app = createApp(App);
-app.use(VueApexCharts);
+export default boot(({ app }) => {
+  app.use(VueApexCharts);
+});
--- src/pages/ProgressPage.vue
+++ src/pages/ProgressPage.vue
@@ -1,33 +1,39 @@
 <template>
-  <LineChart></LineChart>
-  <q-input filled v-model="text" label="BMD" />
-  <q-input filled v-model="text" label="Date" />
-  <q-btn
-    class="q-mt-xl"
-    color="white"
-    text-color="blue"
-    unelevated
-    label="Add Data Point"
-    no-caps
-    padding
-  />
-  <q-btn
-    class="q-mt-xl"
-    color="white"
-    text-color="blue"
-    unelevated
-    to="/"
-    label="Go Home"
-    no-caps
-  />
+  <q-page padding>
+    <q-card> test </q-card>
+
+    <q-input filled v-model="text" label="BMD" />
+    <q-input filled v-model="text" label="Date" />
+    <q-btn
+      class="q-mt-xl"
+      color="white"
+      text-color="blue"
+      unelevated
+      label="Add Data Point"
+      no-caps
+      padding
+    />
+    <q-btn
+      class="q-mt-xl"
+      color="white"
+      text-color="blue"
+      unelevated
+      to="/"
+      label="Go Home"
+      no-caps
+    />
+  </q-page>
 </template>
 <script>
 import { defineComponent, defineAsyncComponent } from 'vue';
+const ApexLine = defineAsyncComponent(() =>
+  import('components/charts/ApexLine.vue')
+);
 
 export default defineComponent({
   name: 'ProgressPage',
   components: {
-    LineChart,
+    ApexLine,
   },
 });
 </script>
