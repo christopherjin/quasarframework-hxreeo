fa44852af3614f1be291d39392bb2d8702209a42
christopherjin <christopherjin@gmail.com>
2022-07-18 13:55:44 -0700

    1 file modified

--- src/components/charts/linechart.vue
+++ src/components/charts/linechart.vue
@@ -13,14 +13,6 @@ export default {
   name: 'linechart',
   data() {
     return {
-      options: {
-        chart: {
-          id: 'vuechart-example',
-        },
-        xaxis: {
-          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
-        },
-      },
       series: [
         {
           name: 'series-1',
